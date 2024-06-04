990f0e3e3ee3eef83828af1c8349e88e74c86a6b
smondal1810 <153348052+smondal1810@users.noreply.github.com>
2024-06-05 00:20:54 +0530

    modify the height config

--- Order-Edge/vite.config.js
+++ Order-Edge/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	server: {
+		host:true,
+		port:3000,
 		proxy: {
 			"/api": {
 				target: "http://localhost:9090",
